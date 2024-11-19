9e3368afa6afbd177195a76ebea7e3803b3bf189
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-11-19 14:14:51 +0400

    settings

---
+++ src/main/java/com/rustam/ci_cd_github_actions/CiCdGithubActionsApplication.java
@@ -0,0 +1,13 @@
+package com.rustam.ci_cd_github_actions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdGithubActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdGithubActionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rustam/ci_cd_github_actions/CiCdGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rustam.ci_cd_github_actions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdGithubActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
