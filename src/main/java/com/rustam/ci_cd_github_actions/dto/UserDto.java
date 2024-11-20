f1833a59591dcb2bfc7719288133233e60699e97
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-11-20 17:22:01 +0400

    Create UserDto.java

---
+++ src/main/java/com/rustam/ci_cd_github_actions/dto/UserDto.java
@@ -0,0 +1,17 @@
+package com.rustam.ci_cd_github_actions.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserDto {
+
+    private String username;
+
+    private String password;
+}
