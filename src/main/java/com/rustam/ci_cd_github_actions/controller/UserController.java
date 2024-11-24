7df9dc9f3e65f4598df74666c1528741caf48f68
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-11-24 16:26:57 +0400

    Update UserController.java

--- src/main/java/com/rustam/ci_cd_github_actions/controller/UserController.java
+++ src/main/java/com/rustam/ci_cd_github_actions/controller/UserController.java
@@ -5,6 +5,7 @@ import com.rustam.ci_cd_github_actions.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class UserController {
 
     private final UserService userService;
 
+    @PostMapping(path = "/create")
     public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto){
         return new ResponseEntity<>(userService.save(userDto), HttpStatus.CREATED);
     }
